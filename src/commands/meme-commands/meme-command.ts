425820d3c4373cd83cbff8776e03937c87efb63c
Catto <71943884+CattoSalad@users.noreply.github.com>
2022-12-11 17:18:25 +0000

    Resolving PR comments

--- src/commands/meme-commands/meme-command.ts
+++ src/commands/meme-commands/meme-command.ts
@@ -1,6 +1,6 @@
 import { SlashCommandBuilder } from 'discord.js';
 import { Context } from '../../discord-bot';
-import * as memeManager from '../../managers/popcord-manager';
+import * as popcordManager from '../../managers/popcord-manager';
 
 export const prefix = 'meme';
 export const options = [];
@@ -8,5 +8,5 @@ export const options = [];
 export const data = new SlashCommandBuilder().setName(prefix).setDescription('Gets a random meme');
 
 export async function execute(ctx: Context) {
-	return memeManager.getSFWMeme({ ctx });
+	return popcordManager.getSFWMeme({ ctx });
 }
