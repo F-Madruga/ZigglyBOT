350a5c57bcd81153055b4117fe69c363fa4defdf
Francisco Madruga <madruga.francisco@protonmail.com>
2022-12-11 20:55:11 +0000

    healthcheck command tests implemented

---
+++ tests/unit/commands/healthcheck-command.test.ts
@@ -0,0 +1,21 @@
+import * as healthcheckCommand from '../../../src/commands/healthcheck-command';
+import { MockDiscordContext } from '../../mocks/discord';
+
+describe('healthcheck command', () => {
+	it('should return the correct command prefix', () => {
+		expect(healthcheckCommand.prefix).toEqual('healthcheck');
+		expect(healthcheckCommand.data.description).toEqual('Replies ok');
+	});
+
+	it('should reply ok', async () => {
+		const mockCtx = new MockDiscordContext({});
+
+		const ctx = mockCtx.getMocked();
+
+		await healthcheckCommand.execute(ctx);
+
+		expect(mockCtx.interaction.mockedResults).toStrictEqual({
+			content: 'ok',
+		});
+	});
+});
